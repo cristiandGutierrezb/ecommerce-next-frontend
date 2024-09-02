0ac425e9638e2246f18588322e4e6620849cd8ee
Cristian Gutierrez <cdgutierrez456@gmail.com>
2024-09-02 09:24:40 -0500

    Initial workspace... Se hace la creacion de primeros componentes y se agrega a cada uno metadatos base

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'About',
+  description: 'This component is the about of my platform'
+}
+
+export default function AboutPage() {
+  return (
+    <div>This is my about</div>
+  )
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'This component is for login on my app'
+}
+
+export default function LoginPage() {
+  return (
+    <div>login</div>
+  )
+}
